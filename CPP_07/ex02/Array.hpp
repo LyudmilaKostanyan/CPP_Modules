410fd21969485cb2f10284779c9c7f43a9eda2ce
Lyudmila <lyudakstnn@gmail.com>
2024-07-15 00:26:21 +0400

    CPP_07/ex02 is started

---
+++ CPP_07/ex02/Array.hpp
@@ -0,0 +1,29 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array
+{
+	private:
+		T				*array;
+		unsigned int	size;
+	public:
+		Array();
+		Array(unsigned int size);
+		Array(const Array &other);
+		Array &operator=(const Array &other);
+		~Array();
+		class IndexException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+		T&		operator[](int index);
+		const T	&operator[](int index) const;
+};
+
+# include "Array.tpp"
+
+#endif
