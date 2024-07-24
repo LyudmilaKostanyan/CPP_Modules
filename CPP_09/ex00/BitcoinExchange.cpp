b88c7e6713976ce522a6acd6b891b87631503a3e
Lyudmila <lyudakstnn@gmail.com>
2024-07-24 17:08:18 +0400

    CPP09/ex00 is done

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -1,8 +1,21 @@
 #include <BitcoinExchange.hpp>
 
-BitcoinExchange::BitcoinExchange()
+bool	check_value(std::string value)
 {
+	int	i = -1;
 
+	if (value.find(".") != value.rfind("."))
+		return (false);
+	while (++i < value.length())
+	{
+		if (i == 0 && (value[i] == '-' || value[i] == '+'))
+			continue ;
+		if (value[i] == '.')
+			continue ;
+		if (value[i] < '0' || value[i] > '9')
+			return (false);
+	}
+	return (true);
 }
 
 bool	check_key(std::string key)
@@ -16,7 +29,7 @@ bool	check_key(std::string key)
 		return (false);
 	if (month < 1 || month > 12)
 		return (false);
-	if (day < 1 || (((month <= 7 && month % 2 == 1) || (month > 7 && month % 2 == 0)) && day > 30)
+	if (day < 1 || (((month <= 7 && month % 2 == 0) || (month > 7 && month % 2 == 1)) && day > 30)
 		|| (month == 2 && (((year - 2008) / 4 == 0 && day > 28) || day > 29)) || day > 31)
 			return (false);
 	return (true);
@@ -28,59 +41,72 @@ bool	check_line(std::string line, int pipe, std::string key, float value)
 		|| (line[pipe - 1] && line[pipe - 1] != ' ')
 		|| (line[pipe + 1] && line[pipe + 1] != ' '))
 	{
-		std::cerr << "Error: bad input => " << line << std::endl;
+		std::cerr << BAD_IN << line << std::endl;
 		return (false);
 	}
-	if ((value == 0 && line.substr(line.find("|") + 1, line.size() - line.find("|") - 1).length() > 9) || value > 1000)
+	if ((value == 0 && line.substr(line.find("|") + 1, line.size()
+		- line.find("|") - 1).length() > 9) || value > 1000)
 	{
-		std::cerr << "Error: too large a number." << std::endl;
+		std::cerr << TOO_LARGE << std::endl;
 		return (false);
 	}
 	if (value <= 0)
 	{
-		std::cerr << "Error: not a positive number." << std::endl;
+		std::cerr << NEG_NUM << std::endl;
 		return (false);
 	}
 	if (!check_key(key))
 	{
-		std::cerr << "Error: wrong date." << std::endl;
+		std::cerr << W_DATA << std::endl;
+		return (false);
+	}
+	if (!check_value(line.substr(pipe + 2, line.size() - pipe - 1)))
+	{
+		std::cerr << W_VALUE << std::endl;
 		return (false);
 	}
 	return (true);
 }
 
-BitcoinExchange::BitcoinExchange(std::string file_name)
+bool	BitcoinExchange::read_data()
 {
-	std::ifstream	database("data.csv");
+	std::ifstream	database(DATABASE);
+	std::string		line;
 
 	if (!database.is_open())
 	{
 		std::cerr << FILE_ERR << std::endl;
-		return ;
+		return (false);
 	}
-	std::string	line;
 	while (std::getline(database, line))
 		data[line.substr(0, line.find(","))] = atof((line.substr(line.find(",")
 			+ 1, line.size() - line.find(",") - 1)).c_str());
 	database.close();
+	return (true);
+}
 
+BitcoinExchange::BitcoinExchange(std::string file_name)
+{
 	std::ifstream	file(file_name);
+	std::string		line;
+	std::string		key;
+	float			value;
+	int				pipe;
 
+	if (!read_data())
+		return ;
 	if (!file.is_open())
 	{
 		std::cerr << FILE_ERR << std::endl;
 		return ;
 	}
-	int		pipe;
-	std::string	key;
-	float	value;
 	std::getline(file, line);
-	if (line != "date | value")
+	if (line != FILE_HEAD)
 	{
-		std::cerr << "Error: wrong format." << std::endl;
+		std::cerr << W_FORMAT << std::endl;
 		return ;
 	}
-	while (std::getline(file, line))
+	while (std::getline(file, line) && line.length() > 0)
 	{
 		pipe = line.find("|");
 		key = line.substr(0, pipe - 1);
@@ -94,7 +120,8 @@ BitcoinExchange::BitcoinExchange(std::string file_name)
 					_key = _key.substr(0, 8) + "0" + std::to_string(atoi(_key.substr(8, 2).c_str()) - 1);
 				else
 				{
-					if (atoi(_key.substr(5, 2).c_str()) > 0 && std::to_string(atoi(_key.substr(5, 2).c_str()) - 1).length() != 2)
+					if (atoi(_key.substr(5, 2).c_str()) > 0
+						&& std::to_string(atoi(_key.substr(5, 2).c_str()) - 1).length() != 2)
 						_key = _key.substr(0, 5) + "0" + std::to_string(atoi(_key.substr(5, 2).c_str()) - 1) + "-31";
 					else if (atoi(_key.substr(5, 2).c_str()) > 0)
 						_key = _key.substr(0, 5) + std::to_string(atoi(_key.substr(5, 2).c_str()) - 1) + "-31";
@@ -108,17 +135,21 @@ BitcoinExchange::BitcoinExchange(std::string file_name)
 	file.close();
 }
 
+BitcoinExchange::BitcoinExchange() {}
+
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
 {
-
+	*this = other;
 }
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
 {
+	if (this != &other)
+	{
+		data.clear();
+		data = other.data;
+	}
 	return *this;
 }
 
-BitcoinExchange::~BitcoinExchange()
-{
-
-}
+BitcoinExchange::~BitcoinExchange() {}
--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -7,8 +7,17 @@
 # include <map>
 # include <cstdlib>
 
+# define DATABASE "data.csv"
 # define DATABASE_ERR "Error: could not open database."
 # define FILE_ERR "Error: could not open file."
+# define BAD_IN "Error: bad input => "
+# define TOO_LARGE "Error: too large a number."
+# define NEG_NUM "Error: not a positive number."
+# define W_DATA "Error: wrong date."
+# define W_VALUE "Error: wrong value."
+# define W_FORMAT "Error: wrong format."
+# define FILE_HEAD "date | value"
+
 
 class BitcoinExchange
 {
@@ -20,6 +29,7 @@ class BitcoinExchange
 		BitcoinExchange(const BitcoinExchange &other);
 		BitcoinExchange &operator=(const BitcoinExchange &other);
 		~BitcoinExchange();
+		bool	read_data();
 };
 
 #endif
